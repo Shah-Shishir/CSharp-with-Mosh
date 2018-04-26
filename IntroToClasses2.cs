f227c9f3ba61fd02dba50d620122c4be781d07bc
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-26 21:11:24 +0600

    Add files via upload

---
+++ IntroToClasses2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IntroToClasses2
+{
+    public class Person
+    {
+        public string name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, name);
+        }
+
+        public static Person Parser (string str)
+        {
+            var person = new Person();
+            person.name = str;
+            return person;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parser("John");
+            person.Introduce("Mosh");
+        }
+    }
+}
