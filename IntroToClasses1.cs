75210c403666d8d5698beaa8c9a630202e578f29
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-26 21:08:04 +0600

    Add files via upload

---
+++ IntroToClasses1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroToClasses
+{
+    public class Person
+    {
+        public string name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hello {0}, I am {1}.", to, name);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            person.name = "Shishir";
+            person.Introduce("John");
+        }
+    }
+}
